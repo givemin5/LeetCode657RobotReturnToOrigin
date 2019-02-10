95f266debc0feb76091fb0d4ccc763991ac2a5f2
givemin5 <givemin5@gmail.com>
2019-02-11 00:57:21 +0800

    Add project files.

---
+++ LeetCode657RobotReturnToOrigin/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LeetCode657RobotReturnToOrigin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCode657RobotReturnToOrigin")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80fe19ac-b3d8-4928-9bb1-bd18fba8e6ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LeetCode657RobotReturnToOrigin/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode657RobotReturnToOrigin
+{
+    public class Solution
+    {
+        public bool JudgeCircle(string moves)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
