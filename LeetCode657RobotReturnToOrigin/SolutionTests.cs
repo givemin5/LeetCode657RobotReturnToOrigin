6bcf5f12d48ab5f3de75f8dd16d5ad6635e4e99a
givemin5 <givemin5@gmail.com>
2019-02-11 00:59:53 +0800

    Add Test

---
+++ LeetCode657RobotReturnToOrigin/SolutionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode657RobotReturnToOrigin
+{
+    [TestClass]
+    public class SolutionTests
+    {
+        [TestMethod]
+        public void Solution_JudgeCircle_UD_ShouldBe_True()
+        {
+            var input = "UD";
+
+            Solution solution = new Solution();
+            var actual = solution.JudgeCircle(input);
+
+            var expected = true;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
